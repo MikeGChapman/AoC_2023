770f4fa49d1328c2cade15cc460e9ade216f320b
MikeGChapman <58981357+MikeGChapman@users.noreply.github.com>
2023-12-04 15:32:26 -0500

    Add files via upload

---
+++ day_02/day02_2.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  1 08:46:18 2023
+
+@author: chapmamg
+"""
+import numpy as np
+
+filename = "inputs.txt"
+
+with open(filename) as f:
+    lines = f.readlines()
+#    print (lines)
+total_1 = 0
+total_2 = 0
+game_number = np.zeros(len(lines),dtype=int)
+red = np.zeros([len(lines),25],dtype=int)
+green = np.zeros([len(lines),25],dtype=int)
+blue = np.zeros([len(lines),25],dtype=int)
+i=0
+
+
+for line in lines:
+    breaker = 0
+    game_number[i]=int(line.split("Game ")[1].split(":")[0])
+    
+    line_cut = line.split(": ")[1].split("\n")[0]
+#    print(line_cut)
+    round_split = line_cut.split("; ")
+#    print(round_split)
+    for j in range(0,len(round_split)):
+        color_split = round_split[j].split(", ")
+        for k in range(0,len(color_split)):
+            if "red" in color_split[k]:
+#                print(color_split[k])
+                red[i,j]=int(color_split[k].split(" ")[0])
+            if "green" in color_split[k]:
+#                print(color_split[k])
+                green[i,j]=int(color_split[k].split(" ")[0])
+            if "blue" in color_split[k]:
+#                print(color_split[k])
+                blue[i,j]=int(color_split[k].split(" ")[0])
+    
+    
+    
+    
+    if max(red[i,:])<13:
+        if max(green[i,:])<14:
+            if max(blue[i,:])<15:
+                total_1 = total_1+game_number[i]
+    total_2 = total_2+ max(red[i,:])*max(green[i,:])*max(blue[i,:])
+    
+    
+    i=i+1          
+print(total_1)
+print(total_2)
